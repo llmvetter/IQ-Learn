d77c8447889ef9dd670adaef9a317bf6ba4b31e2
llmvetter <lennart.vetter@yahoo.de>
2025-02-03 20:19:18 +0100

    Refactors and simplifies SoftQ Class

--- agent/softq.py
+++ agent/softq.py
@@ -1,31 +1,42 @@
 import numpy as np
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 from torch.optim import Adam
 from torch.distributions import Categorical
 
-from agent.softq_models import SimpleQNetwork
-from utils.utils import average_dicts, get_concat_samples
-from iq import iq_loss
+
+class SoftQNetwork(nn.Module):
+    def __init__(self, obs_dim, action_dim, args) -> None:
+        super().__init__()
+        self.args = args
+        self.fc1 = nn.Linear(obs_dim, 128)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, action_dim)
+
+    def forward(self, x) -> torch.Tensor:
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 class SoftQ(object):
-    def __init__(self, batch_size, args):
+    def __init__(self, args):
         self.gamma = args.gamma
-        self.batch_size = batch_size
-        self.device = torch.device(args.device)
         self.args = args
         agent_cfg = args.agent
         self.actor = None
         self.critic_tau = agent_cfg.critic_tau
 
         self.critic_target_update_frequency = agent_cfg.critic_target_update_frequency
-        self.log_alpha = torch.tensor(np.log(agent_cfg.init_temp)).to(self.device)
-        self.q_net = SimpleQNetwork(
+        self.log_alpha = torch.tensor(np.log(agent_cfg.init_temp))
+        self.q_net = SoftQNetwork(
             obs_dim=args.agent.critic_cfg.obs_dim,
             action_dim=args.agent.critic_cfg.action_dim,
             args=args,
         )
-        self.target_net = SimpleQNetwork(
+        self.target_net = SoftQNetwork(
             obs_dim=args.agent.critic_cfg.obs_dim,
             action_dim=args.agent.critic_cfg.action_dim,
             args=args,
@@ -36,7 +47,7 @@ class SoftQ(object):
         self.train()
         self.target_net.train()
 
-    def train(self, training=True):
+    def train(self, training=True) -> nn.Module:
         self.training = training
         self.q_net.train(training)
 
@@ -52,9 +63,13 @@ class SoftQ(object):
     def critic_target_net(self):
         return self.target_net
 
-    def choose_action(self, state, sample=False):
+    def choose_action(
+            self,
+            state: torch.FloatTensor,
+            sample=False
+    ) -> torch.Tensor:
 
-        state = torch.FloatTensor(state).to(self.device).unsqueeze(0)
+        state = torch.FloatTensor(state).unsqueeze(0)
         with torch.no_grad():
             q = self.q_net(state)
             dist = F.softmax(q/self.alpha, dim=1)
@@ -64,140 +79,113 @@ class SoftQ(object):
             else:
                 action = torch.argmax(dist, dim=1)
 
-        return action.detach().cpu().numpy()[0]
+        return action.detach().numpy()[0]
 
-    def getV(self, obs):
+    def getV(
+            self,
+            obs: torch.Tensor
+    ) -> torch.Tensor:
         q = self.q_net(obs)
-        v = self.alpha * \
-            torch.logsumexp(q/self.alpha, dim=1, keepdim=True)
+        v = self.alpha * torch.logsumexp(q/self.alpha, dim=0, keepdim=True)
         return v
-
-    def critic(self, obs, action, both=False):
-        q = self.q_net(obs, both)
-        if isinstance(q, tuple) and both:
-            q1, q2 = q
-            critic1 = q1.gather(1, action.long())
-            critic2 = q2.gather(1, action.long())
-            return critic1, critic2
-
-        return q.gather(1, action.long())
-
-    def get_targetV(self, obs):
+    
+    def get_targetV(
+            self,
+            obs: torch.Tensor,
+    ) -> torch.Tensor:
         q = self.target_net(obs)
-        target_v = self.alpha * \
-            torch.logsumexp(q/self.alpha, dim=1, keepdim=True)
+        target_v = self.alpha * torch.logsumexp(q/self.alpha, dim=0, keepdim=True)
         return target_v
 
-    def update(self, replay_buffer, step):
-        obs, next_obs, action, reward, done = replay_buffer.get_samples(
-            self.batch_size, self.device)
+    def critic(
+            self,
+            obs: torch.Tensor,
+            action: torch.Tensor,
+    ) -> torch.Tensor:
+        q = self.q_net(obs)
+        return q.gather(1, action.long())
 
-        losses = self.update_critic(obs, action, reward, next_obs, done, step)
-
-        if step % self.critic_target_update_frequency == 0:
-            self.target_net.load_state_dict(self.q_net.state_dict())
-
-        return losses
-
-    def update_critic(self, obs, action, reward, next_obs, done, step):
-
+    def q_learn_update(
+            self,
+            obs: torch.Tensor,
+            action: torch.Tensor,
+            reward: torch.Tensor,
+            next_obs: torch.Tensor,
+            done: bool,
+    ) -> dict[str, float]:
+        # Q Learning update of critic network using mse loss
         with torch.no_grad():
             next_v = self.get_targetV(next_obs)
             y = reward + (1 - done) * self.gamma * next_v
 
         critic_loss = F.mse_loss(self.critic(obs, action), y)
-        #print('train_critic/loss', critic_loss, step)
-
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
 
-        return {
-            'loss/critic': critic_loss.item()}
+        return {'loss/critic': critic_loss.item()}
 
-    def iq_update(self, policy_buffer, expert_buffer, step):
-        policy_batch = policy_buffer.get_samples(self.batch_size, self.device)
-        expert_batch = expert_buffer.get_samples(self.batch_size, self.device)
+    # Minimal IQ-Learn objective
+    def iq_learn_update(self, policy_sample, expert_sample):
+        # Inverse Q Learning update of critic network
+        obs, next_obs, action, reward, done, is_expert = get_concat_samples(
+            policy_sample, expert_sample)
 
-        losses = self.iq_update_critic(policy_batch, expert_batch, step)
+        ######
+        # IQ-Learn minimal implementation with X^2 divergence
+        # Calculate 1st term of loss: -E_(ρ_expert)[Q(s, a) - γV(s')]
+        current_Q = self.critic(obs, action)
+        y = (1 - done) * self.gamma * self.getV(next_obs)
 
-        if self.actor and step % self.actor_update_frequency == 0:
-            if not self.args.agent.vdice_actor:
+        reward = (current_Q - y)[is_expert.squeeze(-1)]
+        loss = -(reward).mean()
 
-                if self.args.offline:
-                    obs = expert_batch[0]
-                else:
-                    # Use both policy and expert observations
-                    obs = torch.cat([policy_batch[0], expert_batch[0]], dim=0)
+        # 2nd term for our loss (use expert and policy states): E_(ρ)[Q(s,a) - γV(s')]
+        value_loss = (self.getV(obs) - y).mean()
+        loss += value_loss
 
-                if self.args.num_actor_updates:
-                    for i in range(self.args.num_actor_updates):
-                        actor_alpha_losses = self.update_actor_and_alpha(obs, step)
+        # Use χ2 divergence (adds a extra term to the loss)
+        # Higher alpha leads to more regularization but slower learning
+        chi2_loss = 1/(4 * self.args.method.alpha) * (reward**2).mean()
+        loss += chi2_loss
+        #####
 
-                losses.update(actor_alpha_losses)
-        return losses
-    
-    def iq_update_critic(self, policy_batch, expert_batch, step):
-        args = self.args
-        policy_obs, policy_next_obs, policy_action, policy_reward, policy_done = policy_batch
-        expert_obs, expert_next_obs, expert_action, expert_reward, expert_done = expert_batch
-
-        if args.only_expert_states:
-            # Use policy actions instead of experts actions for IL with only observations
-            expert_batch = expert_obs, expert_next_obs, policy_action, expert_reward, expert_done
-
-        batch = get_concat_samples(policy_batch, expert_batch, args)
-        obs, next_obs, action = batch[0:3]
-
-        agent = self
-        current_V = self.getV(obs)
-        if args.train.use_target:
-            with torch.no_grad():
-                next_V = self.get_targetV(next_obs)
-        else:
-            next_V = self.getV(next_obs)
-
-        if "DoubleQ" in self.args.q_net._target_:
-            current_Q1, current_Q2 = self.critic(obs, action, both=True)
-            q1_loss, loss_dict1 = iq_loss(agent, current_Q1, current_V, next_V, batch)
-            q2_loss, loss_dict2 = iq_loss(agent, current_Q2, current_V, next_V, batch)
-            critic_loss = 1/2 * (q1_loss + q2_loss)
-            # merge loss dicts
-            loss_dict = average_dicts(loss_dict1, loss_dict2)
-        else:
-            current_Q = self.critic(obs, action)
-            critic_loss, loss_dict = iq_loss(agent, current_Q, current_V, next_V, batch)
-
-        # Optimize the critic
+        #optimize critic
         self.critic_optimizer.zero_grad()
-        critic_loss.backward()
-        # step critic
+        loss.backward()
+        #step critic
         self.critic_optimizer.step()
-        return loss_dict
 
-    # Save model parameters
-    def save(self, path, suffix=""):
-        critic_path = f"{path}{suffix}"
-        # print('Saving models to {} and {}'.format(actor_path, critic_path))
-        torch.save(self.q_net.state_dict(), critic_path)
-
-    # Load model parameters
-    def load(self, path, suffix=""):
-        critic_path = f'{path}/{self.args.agent.name}{suffix}'
-        print('Loading models from {}'.format(critic_path))
-        self.q_net.load_state_dict(torch.load(critic_path, map_location=self.device))
+        return loss
 
     def infer_q(self, state, action):
-        state = torch.FloatTensor(state).to(self.device).unsqueeze(0)
-        action = torch.FloatTensor([action]).unsqueeze(0).to(self.device)
+        state = torch.FloatTensor(state).unsqueeze(0)
+        action = torch.FloatTensor([action]).unsqueeze(0)
 
         with torch.no_grad():
             q = self.critic(state, action)
-        return q.squeeze(0).cpu().numpy()
+        return q.squeeze(0).numpy()
 
     def infer_v(self, state):
 
-        state = torch.FloatTensor(state).to(self.device).unsqueeze(0)
+        state = torch.FloatTensor(state).unsqueeze(0)
         with torch.no_grad():
             v = self.getV(state).squeeze()
-        return v.cpu().numpy()
+        return v.numpy()
+    
+
+def get_concat_samples(policy_sample, expert_sample):
+    online_s, online_next_s, online_a, online_r, online_done = policy_sample
+
+    expert_s, expert_next_s, expert_a, expert_r, expert_done = expert_sample
+
+    batch_state = torch.cat([online_s, expert_s], dim=0)
+    batch_next_state = torch.cat([online_next_s, expert_next_s], dim=0)
+    batch_action = torch.cat([online_a, expert_a], dim=0)
+    batch_reward = torch.cat([online_r, expert_r], dim=0)
+    batch_done = torch.cat([online_done, expert_done], dim=0)
+    is_expert = torch.cat([torch.zeros_like(online_r, dtype=torch.bool),
+                           torch.ones_like(expert_r, dtype=torch.bool)], dim=0)
+
+    return batch_state, batch_next_state, batch_action, batch_reward, batch_done, is_expert
+
