38a9d53cfff8e6b30342fd3193aeb0b414146879
llmvetter <lennart.vetter@yahoo.de>
2025-02-21 16:40:20 +0100

    Adds relative speed simulator

--- environment/env.py
+++ environment/env.py
@@ -4,27 +4,36 @@ import gym
 import numpy as np
 import gymnasium as gym
 
+from environment.simulator import Simulator
+
 class CarFollowingEnv(gym.Env):
     def __init__(
             self,
-            max_speed=30,
-            max_distance=100,
+            max_speed: int = 30,
+            max_distance: int = 100,
+            max_rel_speed: int = 75,
             actions: list[float]= [-0.2, -0.1, -0.05, 0, 0.05, 0.1, 0.2],
-            delta_t=0.1,
+            delta_t: float = 0.1,
     ) -> None:
         # Environment parameters
         super().__init__()
         self.max_speed = max_speed
         self.max_distance = max_distance
+        self.max_rel_speed = max_rel_speed
         self.delta_t = delta_t
+        self.simulator = Simulator()
 
         self.action_space = gym.spaces.Discrete(len(actions))
         self.action_mapping = {i: actions[i] for i in range(len(actions))}
 
         # state: [ego speed, distance to lead vehicle]
         self.observation_space = gym.spaces.Box(
-            low=np.array([0, 0]),
-            high=np.array([self.max_speed, self.max_distance]),
+            low=np.array([0, 0, -self.max_rel_speed]),
+            high=np.array([
+                self.max_speed,
+                self.max_distance,
+                self.max_rel_speed,
+            ]),
             dtype=np.float32
         )
         self.state = None
@@ -39,6 +48,7 @@ class CarFollowingEnv(gym.Env):
         self.state = np.array([
             np.random.uniform(5, self.max_speed-5),  # ego speed
             np.random.uniform(5, self.max_distance/2),  # distance to lead vehicle
+            np.random.uniform(-self.max_rel_speed/2, self.max_rel_speed)
         ], dtype=np.float32)
         return self._get_obs(), self._get_info()
 
@@ -48,33 +58,37 @@ class CarFollowingEnv(gym.Env):
             lead_speed: Optional[float] = None,
     ) -> None:
         """Take an action and return the next state, reward, done flag, and additional info."""
+        ### RELATIVE SPEED = V_LEAD - V_FOLLOW ###
 
-        ego_speed, distance_to_lead = self._get_obs()
+        ego_speed, distance_to_lead, relative_speed = self._get_obs()
         acceleration = self.action_mapping.get(action, 0)
 
         # velocity transition
-        ego_speed = np.clip(ego_speed + acceleration * self.delta_t, 0, self.max_speed)
+        next_ego_speed = np.clip(ego_speed + acceleration * self.delta_t, 0, self.max_speed)
 
-        # Gap transition
+        # gap transition
         if lead_speed:
             relative_speed = lead_speed - ego_speed
-            next_distance = distance_to_lead - (relative_speed*self.delta_t) - (0.5*action*self.delta_t**2) #ego(v) - lead(v)
+            next_distance_gap = distance_to_lead + (relative_speed*self.delta_t) - (0.5*action*self.delta_t**2) #ego(v) - lead(v)
+            next_relative_speed = lead_speed - next_ego_speed
         else:
-            relative_speed = np.random.normal(0, 0.8628)
-            distance_next_mean = distance_to_lead - (relative_speed*self.delta_t) - (0.5*action*self.delta_t**2) #ego(v) - lead(v)
-            #next_distance = np.random.normal(loc=distance_next_mean, scale=0.5)
-        next_distance = np.clip(distance_next_mean , 0, self.max_distance)
+            next_distance_gap = distance_to_lead + (relative_speed*self.delta_t) - (0.5*action*self.delta_t**2) #ego(v) - lead(v)
+            # relative speed transition
+            next_relative_speed = self.simulator.smooth_relative_speed(relative_speed)
 
         # Update state
-        self.state = np.array([ego_speed, next_distance], dtype=np.float32)
-
+        self.state = np.array([
+            next_ego_speed,
+            next_distance_gap,
+            next_relative_speed,
+        ], dtype=np.float32)
 
         terminated = False
         truncated = False
         reward = 0
 
         # Episode termination criteria
-        if next_distance < 0.5 or next_distance == self.max_distance:
+        if next_distance_gap < 0.5 or next_distance_gap >= self.max_distance:
             terminated = True
             reward = -1
 
@@ -89,4 +103,5 @@ class CarFollowingEnv(gym.Env):
         return {
             "current_ego_speed": self.state[0],
             "distance_to_lead": self.state[1],
+            "relative_speed": self.state[2],
         }
