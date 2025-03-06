abe3fef8997ffdbcbd71891707dd1868e9393076
llmvetter <lennart.vetter@yahoo.de>
2025-03-06 15:54:23 +0100

    Adjusts requirements, include venv activation

--- batch_job.sh
+++ batch_job.sh
@@ -12,16 +12,7 @@
 #                |--------------------- you can use however many jobs you want. Waitinng time will increase though.
 
 ml purge
-ml release/24.04  GCC/13.2.0 OpenMPI/4.1.6 SciPy-bundle/2023.11
-ml PyTorch/2.1.2
-ml gym[box2d]/0.17.1
-ml gym/0.26.2
-
-# The Task-ID is an increasing integer (in this script from 1 to 24). 
-# You can use it to generate combinations in this script directly, 
-# or use it to index some combinations array/dict in your python code.
-echo $SLURM_ARRAY_TASK_ID
-
-srun python path/to/script.py $SLURM_ARRAY_TASK_ID`
+source /home/h6/leve469a/IQ-Learn/.venv/bin/activate
+srun python /home/h6/leve469a/IQ-Learn/hp_search.py $SLURM_ARRAY_TASK_ID`
 
 
