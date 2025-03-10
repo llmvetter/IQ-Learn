6ce85f77e2a07badce82a58fcbe91ab634eaee79
llmvetter <lennart.vetter@yahoo.de>
2025-03-10 11:14:14 +0100

    Fixes config

--- hp_search.py
+++ hp_search.py
@@ -14,13 +14,18 @@ config_dict = OmegaConf.to_container(config, resolve=True)
 # Define hyperparameter search space
 search_space = {
     'gamma': tune.uniform(0.95, 0.99),
-    'method.alpha': tune.uniform(1, 5),
-    'method.tanh': tune.choice([True, False]),
-    'agent.critic_lr': tune.loguniform(1e-6, 1e-4),
-    'agent.tau': tune.uniform(0.001, 0.05),
+    'method': {
+        'alpha': tune.uniform(1, 5),
+        'tanh': tune.choice([True, False]),
+    },
+    'agent': {
+        'critic_lr': tune.loguniform(1e-6, 1e-4),
+        'tau': tune.uniform(0.001, 0.05),
+    }
 }
 
 merged_config = DotDict({**config_dict, **search_space})
+
 task_id = int(sys.argv[1]) - 1
 
 # Init tuner
@@ -30,7 +35,8 @@ tuner = tune.Tuner(
     tune_config=tune.TuneConfig(
         metric="score",
         mode="max",
-        num_samples=24,
+        num_samples=2,
+        trial_dirname_creator=f"trial_{task_id}",
     ),
 )
 
